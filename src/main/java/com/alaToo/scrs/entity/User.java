a33bd70fbc7ada8a15aa0082654e83441ca312e7
Eldos-05 <eldos.220505@gmail.com>
2025-12-26 00:54:13 +0600

    Add entities

--- src/main/java/com/alaToo/scrs/entity/User.java
+++ src/main/java/com/alaToo/scrs/entity/User.java
@@ -20,7 +20,6 @@ public class User implements UserDetails {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true, nullable = false)
     private String name;
 
     @Column(nullable = false)
